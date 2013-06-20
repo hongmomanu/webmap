deac65264c1cb6dedd724f54df6d73c6cf8d8b06
hongmomanu <hongmomanu@gmail.com>
2013-06-20 10:06:59 +0800

    geoext2

---
+++ static/javascripts/app/view/navigation/mapTree.js
@@ -0,0 +1,52 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: jack
+ * Date: 13-6-10
+ * Time: 下午2:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+Ext.define('CF.view.navigation.mapTree' ,{
+    extend: 'Ext.tree.Panel',
+    alias : 'widget.mapTree',
+     requires: [
+        'Ext.tree.*',
+        'Ext.data.*'
+     ],
+    xtype: 'tree-reorder',
+    
+    
+    //height: 400,
+    //width: 350,
+    title: 'Files',
+    useArrows: true,
+    initComponent: function() {
+        Ext.apply(this, {
+           	store: new Ext.data.TreeStore({
+                proxy: {
+                    type: 'ajax',
+                    url: 'maptree'
+                },
+                root: {
+                    text: '地图浏览',
+                    id: 'src',
+                    expanded: true
+                },
+                folderSort: true,
+                sorters: [{
+                    property: 'text',
+                    direction: 'ASC'
+                }]
+            }),
+            viewConfig: {
+                plugins: {
+                    ptype: 'treeviewdragdrop',
+                    containerScroll: true
+                }
+            }
+
+        });
+        this.callParent(arguments);
+    }
+});
