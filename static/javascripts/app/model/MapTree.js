a2a93e11cd493ae13bfec644eb64df32dccd85e5
hongmomanu <hongmomanu@gmail.com>
2013-06-27 14:52:49 +0800

    geoext2

---
+++ static/javascripts/app/model/MapTree.js
@@ -0,0 +1,10 @@
+Ext.define('CF.model.MapTree', {
+	extend: 'Ext.data.Model',
+    fields: [
+        
+        { name: 'text', type: 'string', mapping: 'text' },
+        { name: 'leaf', type: 'boolean', mapping: 'leaf' },
+        { name: 'expanded', defaultValue: true }
+    ]
+
+})
